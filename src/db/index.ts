63225a26755f1b3527547c60dcd114415a18ad18
Brian Ineza <brianineza01@gmail.com>
2025-06-10 13:12:09 +0200

    setup backend with hono and drizzle

---
+++ src/db/index.ts
@@ -0,0 +1,17 @@
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+
+import env from "@/env";
+
+import * as schema from "./schema";
+
+const client = postgres(env.DATABASE_URL, {
+  max: 10,
+  idle_timeout: 20,
+});
+
+const db = drizzle(client, {
+  schema,
+});
+
+export default db;
