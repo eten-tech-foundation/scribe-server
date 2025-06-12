a5dd211eb199ea8e6280391ce541854a86c43fca
Brian Ineza <brianineza01@gmail.com>
2025-06-12 15:04:38 +0200

    ch: enable cors

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,5 +1,6 @@
 import { OpenAPIHono } from '@hono/zod-openapi';
 import { injectable } from 'inversify';
+import { cors } from 'hono/cors';
 
 import type { AppBindings } from '@/lib/types';
 
@@ -8,6 +9,10 @@ export class Server {
   private _hono = new OpenAPIHono<AppBindings>({
     strict: false,
   });
+
+  constructor() {
+    this._hono.use('*', cors());
+  }
 
   get hono() {
     return this._hono;
