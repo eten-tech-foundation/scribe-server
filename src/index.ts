c0fbf399892bf5806640d3ba7666d0a8859585c9
Brian Ineza <brianineza01@gmail.com>
2025-07-03 14:31:57 +0200

    pino insights

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
+// Import the `useAzureMonitor()` function from the `@azure/monitor-opentelemetry` package.
+// import { useAzureMonitor, AzureMonitorOpenTelemetryOptions } from '@azure/monitor-opentelemetry';
+import { diag, DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api';
+import { ConsoleSpanExporter } from '@opentelemetry/sdk-trace-node';
+
+// Initialize the logger
+diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.ALL);
+
+// const options: AzureMonitorOpenTelemetryOptions = {};
+// // Call the `useAzureMonitor()` function to configure OpenTelemetry to use Azure Monitor.
+// useAzureMonitor(options);
+
 import { serve } from '@hono/node-server';
 
 import app from './app';
