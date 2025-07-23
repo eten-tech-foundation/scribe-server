6bc5dfdd13f1d962b5604fcab607c0e30625df50
vipinpaul <vipin.paul@bridgeconn.com>
2025-07-23 18:35:14 +0530

    re-configured eslint and prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,16 @@
 import antfu from '@antfu/eslint-config';
+import prettier from 'eslint-config-prettier';
 
 export default antfu(
   {
     type: 'app',
     typescript: true,
     formatters: true,
-    stylistic: {
-      indent: 2,
-      semi: true,
-      quotes: 'double',
-    },
+    stylistic: false,
     ignores: ['**/migrations/*'],
   },
   {
+    ...prettier,
     rules: {
       'max-lines': [
         'warn',
